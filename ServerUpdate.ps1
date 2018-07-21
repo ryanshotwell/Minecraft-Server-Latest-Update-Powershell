6b747d7aad789f98e1dd3cbc751ef09ba5a96737
yankeeasdf1 <yankeeasdf1@gmail.com>
2018-07-21 09:36:24 -0400

    Fixes #1 - Script now reads the json files to get the jar download urls

--- ServerUpdate.ps1
+++ ServerUpdate.ps1
@@ -7,26 +7,49 @@ $startServerFile = "ServerStart.bat"
 # # # # # # # # # # # # # # # # # # # # #
 
 
-# Get Version Manifest and convert to PowerShell Json Object
+# Get Version Manifest and convert to PowerShell JSON Object
 $jsonVersions = Invoke-WebRequest -Uri https://launchermeta.mojang.com/mc/game/version_manifest.json | ConvertFrom-Json
 
 # Get the `latest` -> `release` version from JSON object
-$minecraftVersion = $jsonVersions.latest.release
+$minecraftLatestVersion = $jsonVersions.latest.release
 
-# Create a web client
-$webclient = New-Object System.Net.WebClient
+# Get all available versions so we can find the latest's data
+$minecraftVersions = $jsonVersions.versions
 
-# Determine what the server file name will be
-$minecraftJar = "minecraft_server." + $minecraftVersion + ".jar"
+# Loop through each version until we find the latest version
+for ($i=0; $i -lt $minecraftVersions.Count; $i++)
+{
+    # Is this found version id the latest version?
+    if ($minecraftVersions[$i].id -eq $minecraftLatestVersion)
+    {
+        # We found the right version, get its JSON url
+        $jsonUrlLatestVersion = $minecraftVersions[$i].url
+        
+        # Break out of the loop since we have what we need
+        break
+    }
+    else {
+        # Keep searching
+    }
+}
 
-# Determine what the URL is to the latest server jar file
-$url = "https://s3.amazonaws.com/Minecraft.Download/versions/" + $minecraftVersion + "/" + $minecraftJar
+# Get Latest Version Manifest and convert to PowerShell JSON Object
+$jsonLatestVersion = Invoke-WebRequest -Uri $jsonUrlLatestVersion | ConvertFrom-Json
+
+# Get the Server Jar URL
+$jarUrl = $jsonLatestVersion.downloads.server.url
+
+# Determine what the jar file name will be
+$minecraftJar = "minecraft_server." + $minecraftLatestVersion + ".jar"
 
 # Set the location and file name to save the downloaded file to
 $jarPath = $saveJarLocation + $minecraftJar
 
+# Create a web client
+$webclient = New-Object System.Net.WebClient
+
 # Using the webclient, download the file in the $url to $jarPath
-$webclient.DownloadFile($url, $jarPath)
+$webclient.DownloadFile($jarUrl, $jarPath)
 
 
 # If configuration value of $updateStartScript is True, update the Server Start script with new jar file
@@ -39,10 +62,10 @@ if ($updateStartScript)
     $pattern = "minecraft_server.(.*?).jar"
 
     # Find the pattern in the Server Start script
-    $oldMinecraftVersion = [regex]::match($startServerContents, $pattern).Groups[1].Value
+    $oldminecraftLatestVersion = [regex]::match($startServerContents, $pattern).Groups[1].Value
 
     # Replace the found version number with the new version number
-    $startServerContents = $startServerContents -replace $oldMinecraftVersion, $minecraftVersion
+    $startServerContents = $startServerContents -replace $oldminecraftLatestVersion, $minecraftLatestVersion
 
     # Write the new start script to the Server Start script file
     $startServerContents | Set-Content $startServerFile
